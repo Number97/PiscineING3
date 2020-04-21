c2a515d6ddd25e2e2a9743739d5c6c94e5a0726c
Ernest Popovici <>
2020-04-21 22:16:43 +0200

    accueil marche et background enlevés pour mon compte acheteur et vendeur

--- Code/PagesCommunes/JS-Accueil.js
+++ Code/PagesCommunes/JS-Accueil.js
@@ -1,5 +1,6 @@
 function loadItems()
 {
+	var sql
 	//chargement backgrounds et boutons
 	if(!nobody)
 	{
@@ -14,21 +15,34 @@ function loadItems()
 
 		if(session_type=="acheteur")
 		{
+			sql = "select prenom from client where id="+session_id;
 			document.getElementById("navvendre").style.display = "none";
 			document.getElementById("navadmin").style.display = "none";
 		}
 
 		if(session_type=="vendeur")
 		{
+			sql = "select prenom from vendeur where id="+session_id;
 			document.getElementById("navpanier").style.display = "none";
 			document.getElementById("navadmin").style.display = "none";
 		}
 
 		if(session_type=="admin")
 		{
+			sql = "select pseudo from admin where id="+session_id;
 			document.getElementById("navvendre").style.display = "none";
 			document.getElementById("navpanier").style.display = "none";
 		}
+
+		$.get("../PHP-GET.php", {"data" : sql}, (data) => {
+			if (data.match("Error :") != null) { // Error. Reload and ask to connect again ?
+				console.log("ERROR");
+				return;
+			} else {
+				var arr = data.split('\n');
+				document.getElementById("salut").innerHTML="Bienvenue " + ajust(arr[0]) + " !";
+			}
+		});
 	}
 	else
 	{
@@ -36,4 +50,9 @@ function loadItems()
 		document.getElementById("navadmin").style.display = "none";
 		document.getElementById("navpanier").style.display = "none";
 	}
+}
+
+function ajust(str) {
+	if (str == "NULL") return "";
+	return str;
 }
